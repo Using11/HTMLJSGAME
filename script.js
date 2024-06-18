c11993d943eef5e617eb93830222febba3485b3f
Using11 <aryansaran.kalla@gmail.com>
2024-06-18 08:19:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function startGame(){
+  gameCanvas.start();
+}
 
+function updateGame(){
+  
+}
+
+var gameCanvas = {
+  canvas : document.getElementById("gameLoc"),
+  Gcontext : canvas.getcontext("2d"),
+  start : function(){
+    this.animation = setTimeout(requestAnimationFrame(updateGame),1000/30);
+    updateGame();
+  },
+  clear : function(){
+    Gcontext.clearRect(0,0,canvas.width,canvas.height);
+  }
+};
+
+class component {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+gameCanvas.canvas.addEventListener("click", startGame());
