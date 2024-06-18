670e424ffb8a33474dd23b2cdf424103da1a4596
Using11 <aryansaran.kalla@gmail.com>
2024-06-18 08:47:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+var character;
+
 function startGame(){
+  character = new component(100,100,20,20,"rgb(124,120,102)","black",2,false;
   gameCanvas.start();
 }
 
@@ -19,9 +22,37 @@ var gameCanvas = {
 };
 
 class component {
-  constructor(x,y){
+  constructor(x,y,width,height,fillColor,strokeColor,strokeWidth,radiusBool){
     this.x = x;
     this.y = y;
+    this.width = width;
+    this.height = height;
+    this.fillColor = fillColor;
+    gameCanvas.Gcontext.fillStyle = this.fillColor;
+    this.strokeColor = strokeColor;
+    gameCanvas.Gcontext.strokeStyle = this.strokeColor;
+    this.strokeWidth = strokeWidth;
+    gameCanvas.gContext.lineWidth = this.strokeWidth;
+    this.radiusBool = radiusBool;
+    this.rectConstructor = function(){
+      gameCanvas.Gcontext.fillRect(this.x,this.y,this.width,this.height);
+      if (fillColor != false){
+        gameCanvas.Gcontext.strokeRect(this.x,this.y,this.width,this.height);
+      }
+    }
+    this.arcConstructor = function(){
+      gameCanvas.Gcontext.beginPath();
+      gameCanvas.Gcontext.arc(x,y,width,height,360);
+      gameCanvas.Gcontext.closePath();
+    }
+    this.update = function(){
+      if (radiusBool){
+        arcConstructor();
+      }
+      else {
+        rectConstructor();
+      }
+    }
   }
 }
 
